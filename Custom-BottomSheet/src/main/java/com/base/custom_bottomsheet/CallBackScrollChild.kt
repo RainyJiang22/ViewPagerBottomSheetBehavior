b1eb1cf24d8e9ffe70bce75870b5dd78336357a6
jiangshiyu <3434481891@qq.com>
2022-06-14 14:27:19 +0800

    MOD:自定义bottomsheet module,添加jitpack插件

---
+++ Custom-BottomSheet/src/main/java/com/base/custom_bottomsheet/CallBackScrollChild.kt
@@ -0,0 +1,11 @@
+package com.base.custom_bottomsheet
+
+import android.view.View
+
+/**
+ * @author jiangshiyu
+ * @date 2022/5/2
+ */
+interface CallBackScrollChild {
+    fun backScrollChild(scrollChild: View)
+}
